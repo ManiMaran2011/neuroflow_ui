9a56371c55d4993812a541a8ad95721e8f6ee8ce
ManiMaran <20maran21m@gmail.com>
2026-01-15 18:30:46 +0530

    Remove COOP headers to fix Google Login popup

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "unsafe-none",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
+
 
